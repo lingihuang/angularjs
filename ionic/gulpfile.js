67d2443217260091eb50dbf0fcfdafa457294caf
Vivian Huang <lingihuang@hotmail.com>
2016-08-31 16:15:17 +0800

    1. npm install gulp-ng-annotate gulp-useref --save-dev 2. Update path of js files in the index.html 3. Ignore directory www/dist

--- ionic/gulpfile.js
+++ ionic/gulpfile.js
@@ -6,15 +6,19 @@ var sass          = require('gulp-sass');
 var minifyCss     = require('gulp-minify-css');
 var templateCache = require('gulp-angular-templatecache');
 var jshint        = require('gulp-jshint');
+var ngAnnotate    = require('gulp-ng-annotate');
+var useref        = require('gulp-useref');
 var rename        = require('gulp-rename');
 var sh            = require('shelljs');
 
 var paths = {
     sass         : ['./scss/**/*.scss'],
-    templatecache: ['./www/templates/**/*.html']
+    templatecache: ['./www/templates/**/*.html'],
+    ng_annotate  : ['./www/js/*.js'],
+    useref       : ['./www/*.html']
 };
 
-gulp.task('default', ['sass', 'templatecache', 'jshint']);
+gulp.task('default', ['sass', 'templatecache', 'ng_annotate', 'useref', 'jshint']);
 
 gulp.task('sass', function(done) {
     gulp.src('./scss/ionic.app.scss')
@@ -36,6 +40,27 @@ gulp.task('templatecache', function(done) {
         .on('end', done);
 });
 
+gulp.task('ng_annotate', function(done) {
+    gulp.src('./www/js/**/*.js')
+        .pipe(ngAnnotate({single_quotes: true}))
+        .pipe(gulp.dest('./www/dist/dist_js/app'))
+        .on('end', done);
+});
+
+gulp.task('useref', function(done) {
+    // var assets = useref.assets();
+    // gulp.src('./www/*.html')
+    //     .pipe(assets)
+    //     .pipe(assets.restore())
+    //     .pipe(useref())
+    //     .pipe(gulp.dest('./www/dist'))
+    //     .on('end', done);
+    gulp.src('./www/*.html')
+        .pipe(useref())
+        .pipe(gulp.dest('./www/dist'))
+        .on('end', done);
+});
+
 gulp.task('jshint', function(done) {
     return gulp.src('./www/js/**/*.js')
         .pipe(jshint())
@@ -46,6 +71,8 @@ gulp.task('jshint', function(done) {
 gulp.task('watch', function() {
     gulp.watch(paths.sass, ['sass']);
     gulp.watch(paths.templatecache, ['templatecache']);
+    gulp.watch(paths.ng_annotate, ['ng_annotate']);
+    gulp.watch(paths.useref, ['useref']);
 });
 
 gulp.task('install', ['git-check'], function() {
